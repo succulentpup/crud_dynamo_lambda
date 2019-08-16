61982b84a68f6f5d09d672ac07fb6f8c0020659f
succulentpup <pransiddu@gmail.com>
2019-08-16 01:34:24 +0100

    serverless todo application
    This application is implemented using serverless framework.
    This application was deployed to AWS.

---
+++ todos/checked.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.checked = (event, context, callback) => {
+  const todoCompleted = event["queryStringParameters"]["checked"] === 'true';
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Key: {
+      checked: todoCompleted
+    },
+  };
+
+  dynamoDb.scan(params, (error, result) => {
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t fetch the todo item.',
+      });
+      return;
+    }
+
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Items),
+    };
+    callback(null, response);
+  });
+};
+
